9417c0318560d60ca5a458951ef507f6ee67aae1
typeload <samgriesemer@gmail.com>
2017-06-06 21:26:50 -0500

    Update vae.py

--- models/vae.py
+++ models/vae.py
@@ -1,1 +1,38 @@
+class VAE(nn.Module):
+    def __init__(self, ninp):
+        super(VAE, self).__init__()
 
+        self.fc1 = nn.Linear(ninp, 400)
+        self.fc21 = nn.Linear(400, 20)
+        self.fc22 = nn.Linear(400, 20)
+        self.fc3 = nn.Linear(20, 400)
+        self.fc4 = nn.Linear(400, ninp)
+
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def vloss(self, recon_x, x, mu, logvar):
+        BCE = reconstruction_function(recon_x, x)
+        KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
+        KLD = torch.sum(KLD_element).mul_(-0.5)
+
+        return BCE + KLD
+
+    def encode(self, x):
+        h1 = self.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparametrize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        eps = torch.FloatTensor(std.size()).normal_()
+        eps = Variable(eps)
+        return eps.mul(std).add_(mu)
+
+    def decode(self, z):
+        h3 = self.relu(self.fc3(z))
+        return self.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, 784))
+        z = self.reparametrize(mu, logvar)
+        return self.decode(z), mu, logvar
